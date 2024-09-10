b50e7efab6036656c62446053848608988e7ed88
Monisola <bellomonisola867@gmail.com>
2024-09-10 15:42:43 +0100

    make the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -77,7 +77,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       quantity: 1
     });
     }
-    
-    console.log(cart);
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
