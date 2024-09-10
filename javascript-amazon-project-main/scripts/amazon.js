efb18ad87ad60138706bc4765f86df20817bbca0
Monisola <bellomonisola867@gmail.com>
2024-09-10 21:06:40 +0100

    moved add to cart into cart.js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,5 @@
 // console.log('hello');
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 let productsHTML = '';
 
@@ -55,35 +55,23 @@ products.forEach((product) => {
   `;
 });
 
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+function updateCartQunatity() {
+  let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
 
-    let matchingItem;
+    addToCart(productId);
 
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1
-    } else {
-    cart.push({
-      productId: productId,
-      quantity: 1
-    });
-    }
-
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    updateCartQunatity();
   });
 });
