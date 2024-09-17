9f7f3799dad8f91d83063037b4b0168161c11dde
Monisola <bellomonisola867@gmail.com>
2024-09-17 20:42:14 +0100

    removed default tests and added money test

---
+++ javascript-amazon-project-main/jasmine/moneyTest.js
@@ -0,0 +1,17 @@
+import { formatCurrrency } from "../scripts/utils/money.js";
+
+describe('test suite: formatCurrency', () => {
+  it('converts cents into dollars', () => {
+    expect(formatCurrrency(2095)).toEqual('20.95')
+  })
+
+  it('works with zero', () => {
+    expect(formatCurrrency(0)).toEqual('0.00')
+  })
+
+  it('rounds up to the nearest cent', () => {
+    expect(formatCurrrency(2000.5)).toEqual('20.01')
+
+    expect(formatCurrrency(2000.4)).toEqual('20.00')
+  })
+})
