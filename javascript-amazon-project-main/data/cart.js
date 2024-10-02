8b75f7a6f7bca8101b68813dd25831f6616fe67d
Monisola <bellomonisola867@gmail.com>
2024-10-02 17:56:50 +0100

    learned promises and converted code to promises

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -81,3 +81,17 @@ export function removeFromCart (productId) {
 
   saveToStorage()
  }
+
+ export function loadCart (fun) {
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response)
+
+
+    fun()
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart')
+  xhr.send()
+  }
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,3 +1,4 @@
+import { loadCart } from "../data/cart.js";
 import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
@@ -6,7 +7,53 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/car.js'
 // import '../data/backend-practice.js'
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value 1')
+    })
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve('hijo reputa!')
+    })
+  })
+
+]).then((values) => {
   renderOrderSummary();
   renderPaymentSummary();
+  values.forEach((say) => {
+    console.log(say)
+  })
 })
+
+
+
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value 1')
+  })
+
+}).then((value) => {
+  console.log(value)
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve()
+    })
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  })
+})
+*/
+
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   })
+// })
