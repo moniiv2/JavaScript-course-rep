580ef3579fb3937d6024536b29e538e2f641353a
Monisola <bellomonisola867@gmail.com>
2024-10-04 18:42:49 +0100

    created order using the backend

--- javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -1,5 +1,6 @@
 import { cart } from "../../data/cart.js";
 import { getDeliveryOption } from "../../data/deleveryOptions.js";
+import { addOrder } from "../../data/orders.js";
 import { getProduct } from "../../data/products.js";
 import { formatCurrrency } from "../utils/money.js";
 
@@ -55,9 +56,32 @@ export function renderPaymentSummary () {
       </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary
+    js-place-order">
       Place your order
     </button>
   `
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML
+
+  document.querySelector('.js-place-order')
+  .addEventListener('click', async () => {
+    try {
+    const response = await fetch('https://supersimplebackend.dev/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cart: cart
+      })
+    })
+
+    const order = await response.json()
+    addOrder(order)
+   } catch (error) {
+    console.log('unexpected error. try again later.')
+   }
+
+   window.location.href = 'orders.html'
+  })
 }
